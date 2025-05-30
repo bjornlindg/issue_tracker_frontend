c362e5baf0cf711b6b81ff675b4cd9acf086d3bf
bjornlindg <lindgren.bjorn@gmail.com>
2025-05-30 12:43:45 +0200

    Updated GUI CreateUser

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -2,42 +2,44 @@ import { useState } from "react";
 import axios from "axios";
 
 function CreateUser({ onUserCreated }) {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const newUser = { firstName, lastName };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newUser = { firstName, lastName };
 
-    axios.post("http://localhost:8080/api/users", newUser)
-      .then(() => {
-        setFirstName("");
-        setLastName("");
-        onUserCreated();
-      })
-      .catch(error => console.error("Error creating user:", error));
-  };
+        axios.post("http://localhost:8080/api/users", newUser)
+            .then(() => {
+                setFirstName("");
+                setLastName("");
+                onUserCreated();
+            })
+            .catch(error => console.error("Error creating user:", error));
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className="user-form">
-      <h2>Create User</h2>
-      <input
-        type="text"
-        placeholder="First Name"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Last Name"
-        value={lastName}
-        onChange={(e) => setLastName(e.target.value)}
-        required
-      />
-      <button type="submit">Add User</button>
-    </form>
-  );
+    return (
+        <div className="create-user">
+            <h2>Create New User</h2>
+            <form onSubmit={handleSubmit} className="user-form">
+                <input
+                    type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                />
+                <button type="submit">Add User</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateUser;
